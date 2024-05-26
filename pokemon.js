4722411dd03f1427784cbf290424688f36a3b76d
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-05-27 00:01:29 +0100

    update use api and dom

--- pokemon.js
+++ pokemon.js
@@ -2,34 +2,34 @@
 const getPokemon = async () => {
 
     // arrowFunction (funcao anonima)
-    const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/150").then(response => response.json())
-    console.log(pokemon)
-    return pokemon.name
+    const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/5").then(response => response.json())
+    console.log(pokemon)    
+    createPokemonCard(pokemon)
 }
+    getPokemon()
 
-getPokemon().then(pokemons => pokemons)
+    
+    function createPokemonCard(pokemon) {
+       
+    console.log(pokemon.id)
+    
+        const pokemonCards = document.getElementById("pokemon-cards")
+        console.log(pokemonCards)
+    
+        const name = document.createElement("h2")
+        name.textContent = pokemon.name+ ' ' + pokemon.id+ ' ' +pokemon.weight
+        
+       
+       console.log(pokemon.name)
+        console.log(pokemon.weight)
 
-const pokemon = Promise.all([getPokemon()]).then(pokemons => {
-    pokemons.forEach(pokemon => console.log(pokemon));
-});
 
-
-    // const pokemon = {
-    //     "name": "mewtwo",
-    //     "number": 150,
-    //     "hp": 45,
-    //     "attack": 49,
-    //     "defense": 49,
-    //     "special-attack": 65,
-    //     "special-defense": 60,
-    //     "speed": 50
-    // }
-
-    // console.log("object", pokemon);
-    // console.log("name",pokemon.name)
-    // console.log("number",pokemon.number)
-    // console.log(pokemon.name + " " + pokemon.number)
+        pokemonCards.appendChild(name)    
      
+    
+    
+    }
+
     
 
 
